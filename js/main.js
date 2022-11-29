8647f00b278f4eb1a26ec5df26767eb5555f5374
sunset0916 <t09160306@yahoo.co.jp>
2022-11-29 11:46:40 +0900

    ゲームで使う変数をまとめてclassにしました

--- js/main.js
+++ js/main.js
@@ -14,15 +14,108 @@ const resultMessage = document.getElementById('resultMessage');
 
 const radioButton = document.getElementsByName('gameMode');
 
-let gameMode = 0;
-let map;
-let marker;
-let ansLat;
-let ansLng;
-let myLat;
-let myLng;
-let resultLat;
-let resultLng;
+class LocationGame{
+    ansLat;
+    ansLng;
+    myLat;
+    myLng;
+    resultLat;
+    resultLng;
+
+    constructor(){
+        this.gameModes = {
+            PlaceToLatLng: 0,
+            LatLngToPlace: 1
+        }
+        this.gameMode = this.gameModes.PlaceToLatLng;
+    }
+
+    newGame(){
+        this.ansLat = this.generateLat();
+        this.ansLng = this.generateLng();
+        this.myLat = 0;
+        this.myLng = 0;
+        for(let i = 0; i < radioButton.length; i++){
+            if(radioButton.item(i).checked){
+                this.gameMode = radioButton.item(i).value;
+            }
+        }
+    }
+
+    resultGame(){
+        this.resultLat = this.differenceLat(this.ansLat, this.myLat);
+        this.resultLng = this.differenceLng(this.ansLng, this.myLng);
+    }
+
+    generateLat(){
+        if(Math.round(Math.random()) == 0){
+            return Math.round(Math.random() * 90);
+        }else{
+            return Math.round(Math.random() * 90) * -1;
+        }
+    }
+    
+    generateLng(){
+        if(Math.round(Math.random()) == 0){
+            return Math.round(Math.random() * 180);
+        }else{
+            return Math.round(Math.random() * 180) * -1;
+        }
+    }
+
+    differenceLat(aLat,mLat){
+        let rLat = aLat - mLat;
+        if(rLat < 0){
+            rLat *= -1;
+        }
+        return rLat;
+    }
+    
+    differenceLng(aLng,mLng){
+        let rLng;
+        if((aLng < 0 && mLng < 0) || (aLng > 0 && mLng > 0) || aLng == 0 || mLng == 0){
+            rLng = aLng - mLng;
+        }else{
+            let lng0;
+            let lng180;
+            if(aLng < 0){
+                lng0 = mLng - aLng;
+                lng180 = (180 - mLng) - (-180 - aLng);
+            }else{
+                lng0 = aLng - mLng;
+                lng180 = (180 - aLng) - (-180 - mLng);
+            }
+            if(lng0 < lng180){
+                rLng = lng0;
+            }else{
+                rLng = lng180;
+            }
+        }
+        if(rLng < 0){
+            rLng *= -1;
+        }
+        return rLng;
+    }
+}
+
+class Gmaps{
+    map;
+    marker;
+
+    constructor(){}
+
+    createMarker(lat, lng){
+        this.marker.setPosition(null);
+        const markerLatLng = new google.maps.LatLng(lat,lng);
+        this.marker = new google.maps.Marker({
+            position: markerLatLng,
+            map: this.map
+        });
+    }
+}
+
+let locationGame = new LocationGame();
+let gmaps = new Gmaps();
 
 document.body.addEventListener('touchmove', (e) => {
     if (e.touches.length > 1) {
@@ -46,23 +139,22 @@ function initMap() {
         rotateControl: false,
         fullscreenControl: false
     };
-    map = new google.maps.Map(document.getElementById("map"), opts);
+    gmaps.map = new google.maps.Map(document.getElementById("map"), opts);
+    gmaps.marker = new google.maps.Marker({
+        position: new google.maps.LatLng(0,0),
+        map: map
+    });
 }
 
 initMap();
 
-marker = new google.maps.Marker({
-    position: new google.maps.LatLng(0,0),
-    map: map
-});
-
-google.maps.event.addListener(map, 'click', mapListener);
+google.maps.event.addListener(gmaps.map, 'click', mapListener);
 
 function mapListener(event){
-    if(gameMode == 1){
-        myLat = Math.round(event.latLng.lat());
-        myLng = Math.round(event.latLng.lng());
-        marker.setPosition(new google.maps.LatLng(myLat,myLng));
+    if(locationGame.gameMode == locationGame.gameModes.LatLngToPlace){
+        locationGame.myLat = Math.round(event.latLng.lat());
+        locationGame.myLng = Math.round(event.latLng.lng());
+        gmaps.createMarker(locationGame.myLat,locationGame.myLng);
     }
 }
 
@@ -72,7 +164,7 @@ function displaySizeChange(){
     const modalHeaderSizeS = document.getElementById('modal-header-s').clientHeight;
     const modalHeaderSizeR = document.getElementById('modal-header-r').clientHeight;
     let headerSize;
-    if(gameMode == 0){
+    if(locationGame.gameMode == locationGame.gameModes.PlaceToLatLng){
         headerSize = headerPlaceToLatLng.clientHeight;
     }else{
         headerSize = headerLatLngToPlace.clientHeight;
@@ -86,45 +178,21 @@ function displaySizeChange(){
 function startGame(){
     inputLat.value = 0;
     inputLng.value = 0;
-    ansLat = generateLat();
-    ansLng = generateLng();
-    myLat = 0;
-    myLng = 0;
-    for(let i = 0; i < radioButton.length; i++){
-        if(radioButton.item(i).checked){
-            gameMode = radioButton.item(i).value;
-        }
-    }
-    if(gameMode == 0){
+    locationGame.newGame();
+    if(locationGame.gameMode == locationGame.gameModes.PlaceToLatLng){
         headerLatLngToPlace.style.display = 'none';
         headerPlaceToLatLng.style.display = 'block';
-        map.setCenter(new google.maps.LatLng(ansLat,ansLng));
-        createMarker(ansLat,ansLng);
+        gmaps.map.setCenter(new google.maps.LatLng(locationGame.ansLat,locationGame.ansLng));
+        gmaps.createMarker(locationGame.ansLat,locationGame.ansLng);
     }else{
         headerPlaceToLatLng.style.display = 'none';
         headerLatLngToPlace.style.display = 'block';
-        messageLat.textContent = ansLat;
-        messageLng.textContent = ansLng;
-        map.setCenter(new google.maps.LatLng(0,0));
-        createMarker(0,0);
+        messageLat.textContent = locationGame.ansLat;
+        messageLng.textContent = locationGame.ansLng;
+        gmaps.map.setCenter(new google.maps.LatLng(0,0));
+        gmaps.createMarker(0,0);
     }
     modalGameStart.style.display = 'none';
-}
-
-function generateLat(){
-    if(Math.round(Math.random()) == 0){
-        return Math.round(Math.random() * 90);
-    }else{
-        return Math.round(Math.random() * 90) * -1;
-    }
-}
-
-function generateLng(){
-    if(Math.round(Math.random()) == 0){
-        return Math.round(Math.random() * 180);
-    }else{
-        return Math.round(Math.random() * 180) * -1;
-    }
 }
 
 function inputCheckLat(){
@@ -135,7 +203,7 @@ function inputCheckLat(){
     }else if(inputLat.value > 90){
         inputLat.value = 90;
     }
-    myLat = inputLat.value;
+    locationGame.myLat = inputLat.value;
 }
 
 function inputCheckLng(){
@@ -146,58 +214,14 @@ function inputCheckLng(){
     }else if(inputLng.value > 180){
         inputLng.value = 180;
     }
-    myLng = inputLng.value;
-}
-
-function createMarker(lat, lng){
-    marker.setPosition(null);
-    const markerLatLng = new google.maps.LatLng(lat,lng);
-    marker = new google.maps.Marker({
-        position: markerLatLng,
-        map: map
-    });
+    locationGame.myLng = inputLng.value;
 }
 
 function toResult(){
-    resultLat = differenceLat(ansLat,myLat);
-    resultLng = differenceLng(ansLng,myLng);
-    resultMessage.textContent = '緯度' + resultLat + '度、経度' + resultLng + '度の差でした！';
+    locationGame.resultGame();
+    resultMessage.textContent = '緯度' + locationGame.resultLat + '度、経度' + locationGame.resultLng + '度の差でした！';
     modalResult.style.display = 'block';
     displaySizeChange();
-}
-
-function differenceLat(aLat,mLat){
-    let rLat = aLat - mLat;
-    if(rLat < 0){
-        rLat *= -1;
-    }
-    return rLat;
-}
-
-function differenceLng(aLng,mLng){
-    let rLng;
-    if((aLng < 0 && mLng < 0) || (aLng > 0 && mLng > 0) || aLng == 0 || mLng == 0){
-        rLng = aLng - mLng;
-    }else{
-        let lng0;
-        let lng180;
-        if(aLng < 0){
-            lng0 = mLng - aLng;
-            lng180 = (180 - mLng) - (-180 - aLng);
-        }else{
-            lng0 = aLng - mLng;
-            lng180 = (180 - aLng) - (-180 - mLng);
-        }
-        if(lng0 < lng180){
-            rLng = lng0;
-        }else{
-            rLng = lng180;
-        }
-    }
-    if(rLng < 0){
-        rLng *= -1;
-    }
-    return rLng;
 }
 
 function retryGame(){
@@ -207,12 +231,12 @@ function retryGame(){
 }
 
 function postToTwitter(){
-    const shareUrl = 'https://twitter.com/share?url=https://locagame.sunset0916.net&text=私のスコアは%20%0a緯度:' + resultLat + '度%20%0a経度:' + resultLng + '度%20%0aの差でした！%20%0a%20%0a下記URLから緯度経度当てゲームに挑戦！%20%0a';
+    const shareUrl = 'https://twitter.com/share?url=https://locagame.sunset0916.net&text=私のスコアは%20%0a緯度:' + locationGame.resultLat + '度%20%0a経度:' + locationGame.resultLng + '度%20%0aの差でした！%20%0a%20%0a下記URLから緯度経度当てゲームに挑戦！%20%0a';
     window.open(shareUrl);
 }
 
 function copyToClipboard() {
-    const text = '私のスコアは\n緯度:' + resultLat + '度\n経度:' + resultLng + '度\nの差でした！\n\n下記URLから緯度経度当てゲームに挑戦！\nhttps://locagame.sunset0916.net';
+    const text = '私のスコアは\n緯度:' + locationGame.resultLat + '度\n経度:' + locationGame.resultLng + '度\nの差でした！\n\n下記URLから緯度経度当てゲームに挑戦！\nhttps://locagame.sunset0916.net';
     if(navigator.clipboard){
         navigator.clipboard.writeText(text);
     }else{
