4d3a46c56887870875c9c9624c20811b531a5e6c
sunset0916 <t09160306@yahoo.co.jp>
2022-12-05 10:05:00 +0900

    PWAにたぶん対応しました

---
+++ js/sw.js
@@ -0,0 +1,29 @@
+// Cache name
+const CACHE_NAME = 'location-game-cache';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/main.css',
+  './js/main.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
