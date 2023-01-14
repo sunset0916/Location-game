79837bbf2fd48fe301f90503b696eff395bf8baa
sunset0916 <t09160306@yahoo.co.jp>
2023-01-14 11:00:20 +0900

    キャッシュをなんとかするコードを追加しました

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,13 @@
 'use strict';
 const CACHE_NAME = 'location-game-cache';
-const urlsToCache = [
+const CACHE_VERSION = 0.01;
+const CACHE_KEY = `${CACHE_NAME}:${CACHE_VERSION}`;
+const CACHE_FILES = [
 	"./",
 	"./index.html",
 	"./css/main.css",
 	"./js/main.js"
-];
+].map(path => new URL(path, registration.scope).pathname);
 
 self.addEventListener('fetch', function(event) {
 	event.respondWith(async function() {
@@ -23,9 +25,28 @@ self.addEventListener('fetch', function(event) {
 });
 
 self.addEventListener('install', function(event) {
+	console.log("ServiceWorker was installed");
 	event.waitUntil(
 		caches.open(CACHE_NAME).then(function(cache) {
-			return cache.addAll(urlsToCache);
+			console.log("adding cache...");
+			return cache.addAll(CACHE_FILES);
 		})
 	);
+});
+
+self.addEventListener('activate', function(event) {
+	console.log("ServiceWorker has been activated");
+    event.waitUntil(
+        caches.keys().then(function(cacheKeys) {
+            return Promise.all(
+                cacheKeys.filter(function(cacheKey) {
+                    const [cacheName, cacheVersion] = cacheKey.split(':');
+                    return cacheName == CACHE_NAME && cacheVersion != CACHE_VERSION;
+                }).map(function(cacheKey) {
+					console.log("delete cache:" + cacheKey);
+                    return caches.delete(cacheKey);
+                })
+            );
+        })
+    );
 });
