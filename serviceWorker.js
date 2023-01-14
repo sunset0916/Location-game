811a15aff8d2837f427ac6f5fa6d3428ac3f6b09
sunset0916 <t09160306@yahoo.co.jp>
2023-01-14 11:26:22 +0900

    キャッシュが増えまくる問題をなんとかしたつもり

--- serviceWorker.js
+++ serviceWorker.js
@@ -9,19 +9,48 @@ const CACHE_FILES = [
 	"./js/main.js"
 ].map(path => new URL(path, registration.scope).pathname);
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(async function() {
-		try{
-			var res = await fetch(event.request);
-			var cache = await caches.open(CACHE_KEY);
-			cache.put(event.request.url, res.clone());
-			return res;
-		}
-		catch(error){
-			console.log('Using cache');
-			return caches.match(event.request);
-		}
-	}());
+// self.addEventListener('fetch', function(event) {
+// 	event.respondWith(async function() {
+// 		try{
+// 			var res = await fetch(event.request);
+// 			var cache = await caches.open(CACHE_KEY);
+// 			cache.put(event.request.url, res.clone());
+// 			return res;
+// 		}
+// 		catch(error){
+// 			console.log('Using cache');
+// 			return caches.match(event.request);
+// 		}
+// 	}());
+// });
+
+// キャッシュ対象ファイルかどうかを判定する
+const isTargetFile = function(url) {
+    return CACHE_FILES.indexOf(new URL(url).pathname) >= 0;
+};
+
+// スコープ内のページからのリクエストによりfetchイベントが発火する
+this.addEventListener('fetch', function(event) {
+    // レスポンスを宣言する
+    event.respondWith(
+        // cacheStorageの中から管理しているcacheを開く
+        caches.open(CACHE_KEY).then(function(cache) {
+            // cache内にこのリクエストに対するキャッシュが存在するか確認する
+            return cache.match(event.request).then(function(response) {
+                // もしキャッシュがあればそれを返す
+                if (response) return response;
+                // もし無ければネットワークに取得しに行く
+                return fetch(event.request).then(function(response) {
+                    // キャッシュ対象のファイルでキャッシュすべきレスポンスであればキャッシュする
+                    if (isTargetFile(event.request.url) && response.ok) {
+                        cache.put(event.request, response.clone());
+                    }
+                    // レスポンスを返す
+                    return response;
+                });
+            });
+        })
+    );
 });
 
 self.addEventListener('install', function(event) {
